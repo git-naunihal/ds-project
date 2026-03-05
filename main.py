4da43859507083ca1d76b3157b3e52dad5dc2440
Naunihal Singh <email.naunihal.singh@gmail.com>
2026-03-05 15:22:10 +0530

    Add Data Validation Training Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from src.datascience.components import data_validation
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,6 +10,16 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\bx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
