fe4ba71925e880fdbb11f12791460ac30c70c44a
Naunihal Singh <email.naunihal.singh@gmail.com>
2026-03-05 10:59:07 +0530

    Add Data Ingestion Training Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Starting the main.py script")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\bx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
